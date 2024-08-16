8ae19bf713ce700d59e90f4251636c504b645a80
Jorgenettodev <jorgenetto96@gmail.com>
2024-08-16 10:24:11 -0300

    Muda a rota post

--- Listas/Lista02/ex01/src/main/java/com/example/ex01/Ex01Application.java
+++ Listas/Lista02/ex01/src/main/java/com/example/ex01/Ex01Application.java
@@ -24,7 +24,7 @@ public class Ex01Application {
 		return ilhasDeCalor;
 	}
 
-	@PostMapping("/adicionar-ilha/")
+	@PostMapping
 	public String adicionarIlhaDeCalor(@RequestBody IlhaDeCalor ilha) {
 		// IlhaDeCalor novaIlha = (IlhaDeCalor) ilha;
 		ilhasDeCalor.add(ilha);
